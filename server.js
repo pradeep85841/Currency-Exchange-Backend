78e59977716755ea8e3b62fdd016ac27bd4613ad
pradeep <pradeep.kunchala15@gmail.com>
2023-03-11 02:20:08 +0530

    multiple sorces added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const fetch = require("node-fetch");
+const { Headers } = fetch;
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -20,9 +21,9 @@ app.use(function (req, res, next) {
 
 import db from "./dbConnect.mjs";
 
-app.post("/convert", async (req, res) => {
-  let { from, to, amount } = req.body;
+var prices;
 
+const source1 = (from, to, amount) => {
   const key = "8741d8253924eeaccbabc655bdacdd680234d102";
 
   const url = `https://api.getgeoapi.com/v2/currency/convert?api_key=${key}&from=${from}
@@ -37,6 +38,56 @@ app.post("/convert", async (req, res) => {
       });
       res.send(data);
     });
+};
+
+const source2 = async (from, to, amount) => {
+  const url = `https://v6.exchangerate-api.com/v6/0fcf2bdbcbf918143958662b/pair/${from}/${to}/${amount}`;
+
+  try {
+    let response = await fetch(url);
+    return response.json();
+  } catch {
+    (err) => console.error(err);
+  }
+};
+
+const source3 = async (from, to, amount) => {
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "IH9OcI56kSHefCSSr3s5rqBzDZ6PBJhP");
+
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+    headers: myHeaders,
+  };
+  try {
+    let response = await fetch(
+      `https://api.apilayer.com/fixer/convert?to=${to}&from=${from}&amount=${amount}`,
+      requestOptions
+    );
+    return response.json();
+  } catch {
+    (err) => console.error(err);
+  }
+};
+
+/*
+      const dbMongo = db;
+      dbMongo.collection("rescentRequest").insertOne(data, function (err, res) {
+        if (err) throw err;
+      });
+      */
+
+app.post("/convert", async (req, res) => {
+  let { from, to, amount } = req.body;
+  let prices = [0];
+  let resource1 = await source1(from, to, amount);
+  let resource2 = await source2(from, to, amount);
+  let resource3 = await source3(from, to, amount);
+  prices[0] = resource2.conversion_rate;
+  prices[1] = resource3.info.rate;
+  let min_rate = Math.min(...prices);
+  let max_rate = Math.max(...prices);
 });
 
 app.get("/rescentRequest", async (req, res) => {
